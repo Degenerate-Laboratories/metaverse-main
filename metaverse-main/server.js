6e7e3adc6be6462b88eb208515ee2677fff7eb91
battlejoose <battlejoose@gmail.com>
2024-12-21 22:49:31 -0800

    gary 500 health

--- metaverse-main/server.js
+++ metaverse-main/server.js
@@ -418,7 +418,7 @@ io.on('connection', function (socket) {
 		if (currentUser) {
 			gameData.fightStarted = _data;
 			if (_data == "False" && garyNPCClientId && clientLookup[garyNPCClientId]) {
-				clientLookup[garyNPCClientId].health = 50;
+				clientLookup[garyNPCClientId].health = 500;
 			}
 			socket.broadcast.emit('FIGHT_STARTED', _data);
 		}
@@ -592,7 +592,7 @@ function gameloop() {
 			if (u.health < 0) {
 				// Reset npc health after 10s
 				setTimeout(function () {
-					u.health = 50;
+					u.health = 500;
 					if (sockets[u.socketID]) {
 						sockets[u.socketID].emit('UPDATE_HEALTH', u.id, u.health);
 					}
